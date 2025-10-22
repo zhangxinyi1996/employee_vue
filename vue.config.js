f8cada9907ff7de59946c1c8ac3d5aee4e9ef4cc
wanggq080210 <guangw430@gmail.com>
2025-10-22 21:16:05 +0900

    20251022 wanggq ソースmerge

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,17 @@ module.exports = {
       '/img': {  // 代理 /img 开头的请求
         target: 'http://localhost:9999/employee_management',
         changeOrigin: true
+      },
+      // API请求核心代理（自动转发到后端正确接口地址）
+      '^/api': {
+        target: 'http://localhost:9999',
+        changeOrigin: true,
+        pathRewrite: { '^/api': '/employee_management/api' }
+      },
+      // 上传文件访问代理（确保上传的图片能正常显示）
+      '/uploads': {
+        target: 'http://localhost:9999/employee_management',
+        changeOrigin: true
       }
     }
   }
